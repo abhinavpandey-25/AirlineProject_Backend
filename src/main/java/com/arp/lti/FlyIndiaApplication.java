e3da43f30058fa9a17cb47152fd7dd968c156380
abhinavpandey-25 <abhi12345pande@gmail.com>
2022-03-07 11:55:52 +0530

    backend

---
+++ src/main/java/com/arp/lti/FlyIndiaApplication.java
@@ -0,0 +1,14 @@
+package com.arp.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyIndiaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FlyIndiaApplication.class, args);
+	}
+
+
+}
